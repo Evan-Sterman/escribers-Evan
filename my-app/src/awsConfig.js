1b3b4a84df35c3e708209eab8f715ade50295aae
Evan-Sterman <evan.sterman@gmail.com>
2024-12-26 07:10:50 -0500

    Update awsConfig.js

--- my-app/src/awsConfig.js
+++ my-app/src/awsConfig.js
@@ -1,14 +1,10 @@
 import AWS from 'aws-sdk';
-import dotenv from 'dotenv';
-
-// Load environment variables from .env file
-dotenv.config();
 
 // Configure the AWS SDK with credentials and region from environment variables
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID, // Loaded from .env
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // Loaded from .env
-    region: process.env.AWS_REGION // Loaded from .env
+    accessKeyId: 'accesskey', // Replace with your access key ID
+    secretAccessKey: 'secretaccesskey', // Replace with your secret access key
+    region: 'region' // Replace with your bucket's region
 });
 
 const s3 = new AWS.S3(); // Create S3 service object
